aff8ab909dbd7f684bb8da56a554f3650ae50949
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2024-03-28 12:58:30 +0200

    asd

--- WS08_MongoDB/tuntiharjoitus_connect/01_connection.js
+++ WS08_MongoDB/tuntiharjoitus_connect/01_connection.js
@@ -12,7 +12,7 @@ async function main() {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
-
+        console.log("Connected to the mongoDB cluster")
         // Make the appropriate DB calls
         await listDatabases(client);
 
@@ -21,6 +21,7 @@ async function main() {
     } finally {
         // Close the connection to the MongoDB cluster
         await client.close();
+        console.log("Disconnected closed to the mongoDB cluster")
     }
 }
 
